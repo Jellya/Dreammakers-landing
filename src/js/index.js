24c39bf507a0b37665a97af4a6c37f7a82f9caba
Jellya <koipression@gmail.com>
2020-06-18 02:04:01 +0700

    FAQ block

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
 import jQuery from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
+
+var acc = document.getElementsByClassName("FAQ__quest");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var answer = this.nextElementSibling;
+    if (answer.style.maxHeight) {
+        answer.style.maxHeight = null;
+    } else {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+    } 
+  });
+}
